7a3f19bfdfc8657640fb994c82dd4898b378ff20
devMelloww <prostyle114@gmail.com>
2023-10-30 21:53:08 -0700

    adding files

---
+++ src/main/java/org/example/AddNumbers.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class AddNumbers {
+    public int addInts(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        AddNumbers nums = new AddNumbers();
+        System.out.println(nums.addInts(6,4));
+    }
+}
